20b0d2ba529d35db11ae86aebfeefe3fb62d2c41
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-14 16:38:22 +0000

    index.php 2

--- www/index.php
+++ www/index.php
@@ -1,41 +1,40 @@
 <?php
-	@session_start();
+session_start();
 	
-	// Prepend page header 
-	include("../php/header.php");
+/* HEADER */
+include("../php/header.php");
 
-	// Load $pageArray
-	include("../php/pages.php");
+/* Load $pageArray */
+include("../php/pages.php");
 
-	// Check for "page=<type>" in query string
-	// and load content from <type>.php if exists
-	if(isset($_GET["page"]))
-	{
-		if(array_key_exists($_GET["page"], $pageArray))
-		{
-			// If an "id=<id#>" exists, load corresponding <type>id.php
-			if(isset($_GET["id"]))
-			{
-				include($pageArray[$_GET["page"]."id"]);
-				// Get ID from: $_GET["id"]
-			}
-			// Else load main <type>.php
-			else
-			{
-				include($pageArray[$_GET["page"]]);
-			}
+/* Check for "page=<type>" in query string
+ * and load content from <type>.php if exists */
+if(isset($_GET["page"])) {
+
+	if(array_key_exists($_GET["page"], $pageArray)) {
+
+		/* If an "id=<id#>" exists, load corresponding <type>id.php */
+		if(isset($_GET["id"])) {
+
+			include($pageArray[$_GET["page"]."id"]);
+			// To get ID: $_GET["id"]
+
+		/* Load main <type>.php */
+		} else {
+			include($pageArray[$_GET["page"]]);
 		}
-		// Else 404 error
-		else { $_GET["error"] = 404; }
-	}
-	// Check for "search=<keyword>" in query string
-	elseif(isset($_GET["search"]))
-	{
-		// Load search page
-		include("../php/pages/search.php")
-		// Get keyword from $_GET["search"]
-	}
 
-	// Append page footer
-	include("../php/footer.php")
+	/* <type> does not exist */
+	} else {
+		//$_GET["error"] = 404; // Not yet implemented
+	}
+	/* Check for "search=<keyword>" in query string */
+} elseif(isset($_GET["search"])) {
+	/* Load search page */
+	include("../php/pages/search.php");
+	// To get keyword: $_GET["search"]
+}
+
+/* FOOTER */
+include("../php/footer.php");
 ?>
