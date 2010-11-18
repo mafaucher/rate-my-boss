7be9f301c206b25a1707da423382f6f664cf91ee
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-18 04:32:15 +0000

    Menu links & titles 3

--- php/menu.php
+++ php/menu.php
@@ -4,6 +4,16 @@
 
 		<ul>
 <?php
+$titleArray = array(
+"company" => "Companies",
+"evaluation" => "Company Evaluations",
+"document" => "Documents",
+"supervisor" => "Supervisor Evaluations",
+"advertisement" => "Advertisements",
+"siteadmin" => "Site Administration",
+"financeadmin" => "Financial Administration",
+);
+
 	foreach ($titleArray as $key => $title) {
 		print "			<li><a href=index.php?page=$key>$title</a></li>\n";
 	}
