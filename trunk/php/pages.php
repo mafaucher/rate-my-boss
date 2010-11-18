da4c6a267d885a8902eca73df89c06d7abc90111
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-18 04:23:56 +0000

    Proper links & titles in menu 1

--- trunk/php/pages.php
+++ trunk/php/pages.php
@@ -1,20 +1,33 @@
 <?php
 	$pageArray = array(
-		// No ID given (main pages)
+		/* No ID given (main pages) */
 		"company" => "../php/pages/company.php",
 		"evaluation" => "../php/pages/evaluation.php",
 		"document" => "../php/pages/document.php",
 		"supervisor" => "../php/pages/supervisor.php",
-		"advertisement" => "../php/pages/advertisement.php",
 
+		// Privileged -- Only inserted if user is of certain type
+		"advertisement" => "../php/pages/advertisement.php",
 		"siteadmin" => "../php/pages/siteadmin.php",
 		"financeadmin" => "../php/pages/financeadmin.php",
 		
-		// With ID field
+		/* With ID field */
 		"companyid" => "../php/pages/companyid.php",
 		"evaluationid" => "../php/pages/evaluationid.php",
 		"documentid" => "../php/pages/documentid.php",
 		"supervisorid" => "../php/pages/supervisorid.php",
-		"advertisementid" => "../php/pages/advertisementid.php",
+
+		"advertisementid" => "../php/pages/advertisementid.php"
 	);
+
+	$titleArray = array(
+		"company" => "Companies",
+		"evaluation" => "Company Evaluations",
+		"document" => "Documents",
+		"supervisor" => "Supervisor Evaluations",
+		"advertisement" => "Advertisements",
+		"siteadmin" => "Site Administration",
+		"financeadmin" => "Financial Administration",
+	);
+	
 ?>
