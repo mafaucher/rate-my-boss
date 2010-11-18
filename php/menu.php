76f5387a904b5def5326d04aaf6f8ae535d4f21c
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-18 04:38:29 +0000

    Menu links & titles 4

--- php/menu.php
+++ php/menu.php
@@ -4,19 +4,14 @@
 
 		<ul>
 <?php
-$titleArray = array(
-"company" => "Companies",
-"evaluation" => "Company Evaluations",
-"document" => "Documents",
-"supervisor" => "Supervisor Evaluations",
-"advertisement" => "Advertisements",
-"siteadmin" => "Site Administration",
-"financeadmin" => "Financial Administration",
-);
 
-	foreach ($titleArray as $key => $title) {
-		print "			<li><a href=index.php?page=$key>$title</a></li>\n";
-	}
+include_once("../php/pages.php");
+
+foreach ($titleArray as $key => $title) {
+
+	print "			<li><a href=index.php?page=$key>$title</a></li>\n";
+}
+
 ?>
 		</ul>
 
