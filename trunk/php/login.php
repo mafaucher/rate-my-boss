4be26c489527edadc6c99bf1f28348976ab7581d
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-19 19:38:59 +0000

    loginbox 1

--- trunk/php/login.php
+++ trunk/php/login.php
@@ -2,13 +2,21 @@
 
 	<div class="login">
 
-		<form action="" method="post">
-        	<p>Username:&nbsp;<input class="loginbox" type="text" name="username" /></p>
-			<p>Password:&nbsp;<input class="loginbox" type="password" name="password" /></p>
-			<p>Remember me&nbsp;<input type="checkbox" name="remember" />
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			<input type="submit" name="sublogin" value="Login" /></p>
-		</form>
+<?php
+
+	if (!$_SESSION["logged"]) {
+		echo('		<form action="" method="post">');
+		echo('			<p>Username:&nbsp;<input class="loginbox" type="text" name="username" /></p>');
+		echo('			<p>Password:&nbsp;<input class="loginbox" type="password" name="password" /></p>');
+		echo('			<p>Remember me&nbsp;<input type="checkbox" name="remember" />');
+		echo('			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+		echo('			<input type="submit" name="sublogin" value="Login" /></p>');
+		echo('		</form>');
+	}
+	else {
+		echo("<p>Welcome$_SESSION['username']!</p>");
+	}
+?>
 
 	</div>
 
