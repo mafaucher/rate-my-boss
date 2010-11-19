8e57a25f3f8abb002c59aa1eca5f5a9da3e542ba
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-19 23:49:35 +0000

    logout 1

--- trunk/php/logout.php
+++ trunk/php/logout.php
@@ -3,5 +3,5 @@
 	setcookie("username", $username, time()-60*60*24*100, "/");
 	setcookie("password", $password, time()-60*60*24*100, "/");
 	session_destroy(); 	
-	@header("Location: ../www/index.php");
+	//@header("Location: ../www/index.php");
 ?>
