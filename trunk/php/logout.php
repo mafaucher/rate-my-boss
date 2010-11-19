77bc2dc7f02db97f3e85304762dfbadeff7c4f61
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-19 23:59:27 +0000

    logout 2

--- trunk/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Negate the cookies, destroy the session, redirect to index.php.
-	setcookie("username", $username, time()-60*60*24*100, "/");
-	setcookie("password", $password, time()-60*60*24*100, "/");
-	session_destroy(); 	
-	//@header("Location: ../www/index.php");
-?>
