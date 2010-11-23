091a2b07f0c996d62e6617c81a61563499cdfef6
pete.rockwell@gmail.com <pete.rockwell@gmail.com@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-23 09:43:10 +0000

    creation script updated - only a few more tables to complete. Need to discuss queries for tags table.

--- sql/creationScript.sql
+++ sql/creationScript.sql
@@ -3,7 +3,6 @@
 # ORGANIZATION
 
 drop table organization;
-
 create table organization
 (
 orgId INT NOT NULL AUTO_INCREMENT,
@@ -17,7 +16,138 @@ isPending INT NOT NULL,
 PRIMARY KEY (orgId)
 );
 
+# SUPERVISOR
+
+drop table supervisor;
+create table supervisor
+(
+superId INT NOT NULL AUTO_INCREMENT,
+orgId INT NOT NULL,
+title VARCHAR(40) NOT NULL,
+levelOfAuthority INT,
+isPending INT NOT NULL,
+PRIMARY KEY (superId)
+);
+
+# DOCUMENT
+
+drop table document;
+create table document
+(
+docId INT NOT NULL AUTO_INCREMENT,
+orgId INT NOT NULL,
+title VARCHAR(40) NOT NULL,
+reported INT NOT NULL,
+PRIMARY KEY (docId)
+);
+
+# RATING
+
+drop table rating;
+create table rating
+(
+ratingId INT NOT NULL AUTO_INCREMENT,
+orgId INT NOT NULL,
+socialValues INT,
+professionalism INT,
+openness INT,
+encouraging INT,
+acceptance INT,
+recognition INT,
+qualityWorkplace INT,
+fairness INT,
+cooperation INT,
+rewardSystem INT,
+fairWages INT,
+qualityBenefits INT,
+supportEmployees INT,
+levelStress INT,
+levelCollegiality INT,
+levelBureaucracy INT,
+advancement INT,
+supportFamily INT,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (ratingId)
+);
+
+# ORGANIZATION EVALUATION
+
+drop table orgEvaluation;
+create table orgEvaluation
+(
+orgEvalId INT NOT NULL AUTO_INCREMENT,
+orgId INT NOT NULL,
+text VARCHAR(600) NOT NULL,
+reported INT NOT NULL,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (orgEvalId)
+);
+
+# SUPERVISOR EVALUATION
+
+drop table superEvaluation;
+create table superEvaluation
+(
+superEvalId INT NOT NULL AUTO_INCREMENT,
+superId INT NOT NULL,
+text VARCHAR(600) NOT NULL,
+reported INT NOT NULL,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (superEvalId)
+);
+
+# ORGANIZATION COMMENT
+
+drop table orgComment;
+create table orgComment
+(
+orgCommentId INT NOT NULL AUTO_INCREMENT,
+orgEvaluationId INT NOT NULL,
+text VARCHAR(600) NOT NULL,
+reported INT NOT NULL,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (orgCommentId)
+);
+
+# SUPERVISOR COMMENT
+
+drop table superComment;
+create table superComment
+(
+superCommentId INT NOT NULL AUTO_INCREMENT,
+superEvaluationId INT NOT NULL,
+text VARCHAR(600) NOT NULL,
+reported INT NOT NULL,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (superCommentId)
+);
+
+# SUPERVISOR COMMENT
+
+drop table superComment;
+create table superComment
+(
+superCommentId INT NOT NULL AUTO_INCREMENT,
+superEvaluationId INT NOT NULL,
+text VARCHAR(600) NOT NULL,
+reported INT NOT NULL,
+uString CHAR(32) NOT NULL,
+PRIMARY KEY (superCommentId)
+);
+
+# TAG
+
+drop table tag;
+create table tag
+(
+tagId INT NOT NULL AUTO_INCREMENT,
+parentId INT NOT NULL,
+type VARCHAR(40) NOT NULL,
+keyword VARCHAR(40) NOT NULL,
+PRIMARY KEY (tagId)
+);
+
+# INSERT VALUES
+
 insert into organization (name, industryType, city, province, website, numberofEmployees, isPending) values ('Bell','telecommunications', 'Montreal', 'Quebec', 'www.bell.ca', 843, 1),
 ('Google','web', 'Irvine', 'California', 'www.google.ca', 307, 1);
-
-
