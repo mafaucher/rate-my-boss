5b686e11323e8608804c5060d016091b870fd317
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-27 15:03:17 +0000

    trying to fix session

--- trunk/php/session.php
+++ trunk/php/session.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 /* Set login cookie (No sure all this is still needed)*/
 if($_SESSION['writeCookies']) {
 	setcookie("username", $_SESSION['username'], time()+60*60*24*100, "/");
