0c61034342910982f6c4057b42686abfb67a97df
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-11-30 21:31:16 +0000

    updatedb.sh script

--- updatedb.sh
+++ updatedb.sh
@@ -1,1 +1,1 @@
-mysql5 -h clipper.encs.concordia.ca -u tjc353_2 -p ahf3HY tjc353_2 < sql/creationScript.sql
+mysql5 -h clipper.encs.concordia.ca -u tjc353_2 -pahf3HY tjc353_2 < sql/creationScript.sql
