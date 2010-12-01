1612e3fff35367cc55f60b5c401f577c4b13d404
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-12-01 18:27:23 +0000

    checklogin now works with database

--- www/logout.php
+++ www/logout.php
@@ -3,8 +3,8 @@
 /* Negate the cookies, destroy the session, redirect to index.php. */
 setcookie("username", $username, time()-60*60*24*100, "/");
 setcookie("password", $password, time()-60*60*24*100, "/");
-//session_start();
-session_destroy();
+@session_start();
+@session_destroy();
 @header("Location: index.php");
 
 ?>
