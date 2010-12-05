1e4b96bcaf3aa0f17d5a014b250c482edfe5639b
pete.rockwell@gmail.com <pete.rockwell@gmail.com@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-12-05 00:30:30 +0000

    suggestorg form implemented note: no value checking on input fields yet

--- trunk/php/pages/organization.php
+++ trunk/php/pages/organization.php
@@ -26,14 +26,32 @@ while($row = mysql_fetch_array($result))
     </li>";
 }
 echo "
+</ul>
 <br />
 <a href='index.php?page=suggestorg'><button type='button'>Suggest an Organization</button></a> <br />
+<br />
 ";
 if($HTTP_SERVER_VARS['REQUEST_METHOD']=='POST'){
-echo "<span class='score'>Thanks for suggesting an organization!</span> It will be pending until an administrator can confirm it.";
+
+$name = $_POST['name'];
+$industryType = $_POST['industryType'];
+$city = $_POST['city'];
+$province = $_POST['province'];
+$website = $_POST['website'];
+$numberofEmployees = $_POST['numberofEmployees'];
+
+$sql="insert into organization (name, industryType, city, province, website, numberofEmployees, isPending) values
+('$name', '$industryType', '$city', '$province', '$website', '$numberofEmployees', 1)";
+
+//$sql="insert into organization (name) values ($name, $industryType, $city, $province, 
+
+//echo "$name, $industryType, $city, $province, $website, $numberofEmployees";
+mysql_query($sql);
+
+echo "<span class='score'>Thanks for suggesting an organization!</span><br />
+It will be pending until an administrator can confirm it.";
 }
 
 include "../php/closedb.php";
 ?>
-</ul>
 </div>
