88f2efeeaba6a61588cc66bd7987c51c8aabfd29
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-12-10 00:32:54 +0000

    ads update hits

--- php/pages/search.php
+++ php/pages/search.php
@@ -18,7 +18,12 @@ include "../php/opendb.php";
 
 /* Get the first, least recently used and active AD corresponding to keyword */
 
-$query = "SELECT content, name, website FROM ad NATURAL JOIN (tag, business) WHERE tag.keyword='$_GET[search]' AND NOT ad.isPending AND ad.counter>0 ORDER BY ad.lastView LIMIT 1";
+$query = "SELECT content, name, website FROM ad a JOIN (tag t, business b)
+	ON (a.adId=t.adId AND a.userId=b.userId)
+	WHERE t.keyword='$_GET[search]' AND NOT a.isPending AND a.counter>0
+	ORDER BY a.lastView
+	LIMIT 1";
+
 $result = mysql_query($query);
 
 if ($row = mysql_fetch_array($result)) {
@@ -27,7 +32,11 @@ if ($row = mysql_fetch_array($result)) {
 		<p><small>An advertisement from our sponsors: <strong><a href='$row[website]'>$row[name]</a></strong></p>
 		<br />
 		";
+
 	/* Update ad */
+
+	$subquery = "UPDATE ad SET counter = counter-1, hits = hits - 1, lastView = CURRENT_TIMESTAMP()";
+	mysql_query($subquery);
 }
 
 include "../php/closedb.php";
