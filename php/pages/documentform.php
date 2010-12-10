4247619b00827506c9ded7156f39881495a98366
pete.rockwell@gmail.com <pete.rockwell@gmail.com@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-12-10 21:22:01 +0000

    added document form

---
+++ php/pages/documentform.php
@@ -0,0 +1,14 @@
+<div class="main">
+	<form action="index.php?page=documentupload" method="post" enctype="multipart/form-data">
+		<br />
+		<label for="title">Title:</label>
+		<input type='text' name='title' />
+		<br />
+		<br />
+		<label for="file">Filename:</label>
+		<input type="file" name="file" id="file" />
+		<br />
+		<br />
+		<input type="submit" name="submit" value="Submit" />
+	</form>
+</div>
