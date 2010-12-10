b4f81445e44bd17db1abfaa4767d3d200385ea19
ma.faucher <ma.faucher@e613623c-1d9e-062f-0c07-3e89e16435e6>
2010-12-10 10:22:41 +0000

    ad keywords 6

--- php/checkad.php
+++ php/checkad.php
@@ -36,9 +36,8 @@ if (isset($_POST['content'])) {
 
 	for ($i = 1; $i <= 10; $i++) {
 		if ($keyword[$i] != "") {
-			echo $adId;
-			echo $keyword[$i];
-			$query = "INSERT INTO tag (adId, keyword) VALUE ($adId, ${keyword[$i]})";
+			$word = $keyword[$i];
+			$query = "INSERT INTO tag (adId, keyword) VALUE ($adId, $word)";
 			echo mysql_query($query);
 		}
 	}
